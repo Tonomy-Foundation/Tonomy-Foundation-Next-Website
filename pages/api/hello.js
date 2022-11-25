a54fa122390db3977e2afb91394e3e9e317aa09d
Aymen <aymenebrahim15@gmail.com>
2022-11-25 19:26:34 +0100

    Updating test on master

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,12 @@
 // Next.js Edge API Routes: https://nextjs.org/docs/api-routes/edge-api-routes
 
-export const config = {  runtime: 'experimental-edge',
-}
+export const config = { runtime: "experimental-edge" };
 
-export default async function (req) {  return new Response(    JSON.stringify({ name: 'John Doe' }),    {      status: 200,      headers: {        'Content-Type': 'application/json'      }    }  )
+export default async function (req) {
+  return new Response(JSON.stringify({ name: "John Doe" }), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
