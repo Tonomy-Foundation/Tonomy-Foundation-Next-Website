d9c66ed5456589c13ba86542c5ddb44111c23c3e
Aymen <aymenebrahim15@gmail.com>
2022-12-05 13:33:12 +0100

    Reduced build size

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,6 @@
 import Navbar from '../components/Layouts/Navbar'
 import MainBanner from '../components/HomeOne/MainBanner'
 import Features from '../components/HomeOne/Features'
-import About from '../components/HomeOne/About'
-import WhyChooseUs from '../components/HomeOne/WhyChooseUs'
-import Services from '../components/HomeOne/Services'
-import MakeYourBusiness from '../components/Common/MakeYourBusiness'
-import WhatWeOffer from '../components/HomeOne/WhatWeOffer'
-import Testimonials from '../components/Common/Testimonials'
-import News from '../components/Common/News'
 import Footer from '../components/Layouts/Footer'
 
 const Index = () => {
@@ -18,20 +11,6 @@ const Index = () => {
             <MainBanner />
 
             <Features />
-
-           {/*<About />*/}
-
-            {/*<WhyChooseUs />*/}
-
-            {/*<Services />*/}
-
-            {/*<MakeYourBusiness />*/}
-
-            {/*<WhatWeOffer />*/}
-
-            {/*<Testimonials />*/}
-
-            {/*<News />*/}
             
             <Footer />
         </>
