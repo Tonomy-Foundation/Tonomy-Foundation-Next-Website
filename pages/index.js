f60ffada179111c6aa611954f760a72749175479
Aymen <aymenebrahim15@gmail.com>
2022-12-05 14:17:22 +0100

    Added components from development branch

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 import Navbar from '../components/Layouts/Navbar'
 import MainBanner from '../components/HomeOne/MainBanner'
 import Features from '../components/HomeOne/Features'
+import About from '../components/AboutOne/About'
+import WhyChooseUs from '../components/HomeOne/WhyChooseUs'
+import Services from '../components/HomeOne/Services'
+import MakeYourBusiness from '../components/Common/MakeYourBusiness'
+import WhatWeOffer from '../components/HomeOne/WhatWeOffer'
+import Testimonials from '../components/Common/Testimonials'
+import News from '../components/Common/News'
 import Footer from '../components/Layouts/Footer'
 
 const Index = () => {
