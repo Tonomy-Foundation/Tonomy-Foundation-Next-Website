89327ad8954275cebf5eeba26fede31590139557
Aymen <34170109+Riae@users.noreply.github.com>
2023-01-20 10:12:17 +0100

    Added google code

--- pages/_app.js
+++ pages/_app.js
@@ -1,56 +1,76 @@
-import '../styles/bootstrap.min.css';
-import 'animate.css'
-import '../styles/boxicons.min.css';
-import '../styles/flaticon.css';
-import 'react-accessible-accordion/dist/fancy-example.css';
+import "../styles/bootstrap.min.css";
+import "animate.css";
+import "../styles/boxicons.min.css";
+import "../styles/flaticon.css";
+import "react-accessible-accordion/dist/fancy-example.css";
 import "swiper/css";
 import "swiper/css/bundle";
 
 // Global styles
-import '../styles/style.css';
-import '../styles/responsive.css';
+import "../styles/style.css";
+import "../styles/responsive.css";
 
-import App from 'next/app';
-import Head from 'next/head';
-import Loader from '../components/Shared/Loader';
-import GoTop from '../components/Shared/GoTop';
+import App from "next/app";
+import Head from "next/head";
+import Loader from "../components/Shared/Loader";
+import GoTop from "../components/Shared/GoTop";
+import script from "next/script";
 
 export default class MyApp extends App {
-
-    // Preloader
-    state = {
-        loading: true
-    };
-    componentDidMount() {
-        this.timerHandle = setTimeout(() => this.setState({ loading: false }), 2000); 
+  // Preloader
+  state = {
+    loading: true,
+  };
+  componentDidMount() {
+    this.timerHandle = setTimeout(
+      () => this.setState({ loading: false }),
+      2000
+    );
+  }
+  componentWillUnmount() {
+    if (this.timerHandle) {
+      clearTimeout(this.timerHandle);
+      this.timerHandle = 0;
     }
-    componentWillUnmount() {
-        if (this.timerHandle) {
-            clearTimeout(this.timerHandle);
-            this.timerHandle = 0;
-        }
-    }
-    
-    render () {
-        const { Component, pageProps } = this.props
-        return (
-            <>
-                <Head>
-                    <meta 
-                        name="viewport" 
-                        content="width=device-width, initial-scale=1" 
-                    />
-                    <title>Tonomy Foundation</title>
-                </Head>
+  }
 
-                <Component {...pageProps} />
-                
-                {/* Preloader */}
-                <Loader loading={this.state.loading} />
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <>
+        <Head>
+              
+          <Script
+            strategy="afterInteractive"
+            src="https://www.googletagmanager.com/gtag/js?id=G-R99KT7GD5R"
+          />
+              
+          <Script
+            id="google-analytics"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+             window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XXXXXXX', {
+            page_path: window.location.pathname,
+            });
+             `,
+            }}
+          />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>Tonomy Foundation</title>
+        </Head>
 
-                {/* Go Top Button */}
-                <GoTop scrollStepInPx="100" delayInMs="10.50" />
-            </>
-        );
-    }
+        <Component {...pageProps} />
+
+        {/* Preloader */}
+        <Loader loading={this.state.loading} />
+
+        {/* Go Top Button */}
+        <GoTop scrollStepInPx="100" delayInMs="10.50" />
+      </>
+    );
+  }
 }
