e4953829c48dee27b69fdd5eb180387a958724e7
Xdsylar <566460@edu.rocmn.nl>
2023-01-24 15:39:06 +0100

    made the style only affect TonomyID page

--- components/tonomy-id/Features-TonomyID.js
+++ components/tonomy-id/Features-TonomyID.js
@@ -13,7 +13,7 @@ const TIDfeatures = () => {
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-3">
-                            <div className="single-features">
+                            <div className="single-features2">
                                 <i className="flaticon-cloud-computing-1"></i>
                                 <h3>Verifiable credentials</h3>
                                 <p>Verifiable credentials (VCs) are an open standard for digital credentials.
@@ -23,7 +23,7 @@ const TIDfeatures = () => {
                         </div>
 
                         <div className="col-lg-3">
-                            <div className="single-features">
+                            <div className="single-features2">
                                 <i className="flaticon-engineer"></i>
                                 <h3>Peer-to-peer communication</h3>
                                 <p>Peer-to-peer (P2P) computing or networking is a distributed application architecture that partitions tasks or workloads between peers. Peers are equally privileged, equipotent participants in the network. They are said to form a peer-to-peer network of nodes.</p>
@@ -31,42 +31,42 @@ const TIDfeatures = () => {
                         </div>
 
                         <div className="col-lg-3">
-                            <div className="single-features">
+                            <div className="single-features2">
                                 <i className="flaticon-success"></i>
                                 <h3>Smart contracts / Transaction signing</h3>
                                 <p>The electronic signature is a solution that allows you to sign any document (contracts, estimates, agreements, etc.) electronically. It has many advantages including speeding up the processes related to the signing of a document and digitally archiving these signed documents</p>
                             </div>
                         </div>
                         <div className="col-lg-3">
-                            <div className="single-features">
+                            <div className="single-features2">
                                 <i className="flaticon-success"></i>
                                 <h3>White labelled</h3>
                                 <p>A white-label product is a product or service produced by one company (the producer) that other companies (the marketers) rebrand to make it appear as if they had made it. The name derives from the image of a white label on the packaging that can be filled in with the marketer's trade dress. White label products are sold by retailers with their own trademark but the products themselves are manufactured by a third party.</p>
                             </div>
                         </div>
                         <div className="col-lg-3">
-                            <div className="single-features">
+                            <div className="single-features2">
                                 <i className="flaticon-success"></i>
                                 <h3>Built in MFA (multi factor authentication)</h3>
                                 <p>Multi-factor authentication (MFA or 2FA) is an electronic authentication method in which a user is granted access to a website or application only after successfully presenting two or more pieces of evidence (or factors) to an authentication mechanism</p>
                             </div>
                         </div>
                         <div className="col-lg-3">
-                            <div className="single-features">
+                            <div className="single-features2">
                                 <i className="flaticon-success"></i>
                                 <h3>No Single Point of Failure</h3>
                                 <p>A single point of failure (SPOF) is a part of a system that, if it fails, will stop the entire system from working.SPOFs are undesirable in any system with a goal of high availability or reliability, be it a business practice, software application, or other industrial system.</p>
                             </div>
                         </div>
                         <div className="col-lg-3">
-                            <div className="single-features">
+                            <div className="single-features2">
                                 <i className="flaticon-success"></i>
                                 <h3>Cross-platform</h3>
                                 <p>In computing, cross-platform software is computer software that is designed to work in several computing platforms. Some cross-platform software requires a separate build for each platform, but some can be directly run on any platform without special preparation.</p>
                             </div>
                         </div>
                         <div className="col-lg-3">
-                            <div className="single-features">
+                            <div className="single-features2">
                                 <i className="flaticon-success"></i>
                                 <h3>Low code / No code</h3>
                                 <p>Low-code and no-code development platforms are tools for people who either do not know how to code or have no time to code. Whereas these low-code and no-code frameworks are built on actual coding languages like PHP, Python, and Java, end users are not concerned with the specifics</p>
