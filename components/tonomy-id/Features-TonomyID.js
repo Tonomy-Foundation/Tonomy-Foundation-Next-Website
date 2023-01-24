7b5bd8b1329d77b870d96e0fd64944ef5403b445
Xdsylar <566460@edu.rocmn.nl>
2023-01-24 16:32:02 +0100

    added fitting icons to Features

--- components/tonomy-id/Features-TonomyID.js
+++ components/tonomy-id/Features-TonomyID.js
@@ -14,7 +14,7 @@ const TIDfeatures = () => {
                     <div className="row">
                         <div className="col-lg-3">
                             <div className="single-features2">
-                                <i className="flaticon-cloud-computing-1"></i>
+                                <i className="flaticon-chip"></i>
                                 <h3>Verifiable credentials</h3>
                                 <p>Verifiable credentials (VCs) are an open standard for digital credentials.
                                     They can represent information found in physical credentials, such as a passport or license, as well as new things that have no physical equivalent, such as ownership of a bank account.
@@ -32,42 +32,42 @@ const TIDfeatures = () => {
 
                         <div className="col-lg-3">
                             <div className="single-features2">
-                                <i className="flaticon-success"></i>
+                                <i className="flaticon-blockchain"></i>
                                 <h3>Smart contracts / Transaction signing</h3>
                                 <p>The electronic signature is a solution that allows you to sign any document (contracts, estimates, agreements, etc.) electronically. It has many advantages including speeding up the processes related to the signing of a document and digitally archiving these signed documents</p>
                             </div>
                         </div>
                         <div className="col-lg-3">
                             <div className="single-features2">
-                                <i className="flaticon-success"></i>
+                                <i className="flaticon-choice"></i>
                                 <h3>White labelled</h3>
                                 <p>A white-label product is a product or service produced by one company (the producer) that other companies (the marketers) rebrand to make it appear as if they had made it. The name derives from the image of a white label on the packaging that can be filled in with the marketer's trade dress. White label products are sold by retailers with their own trademark but the products themselves are manufactured by a third party.</p>
                             </div>
                         </div>
                         <div className="col-lg-3">
                             <div className="single-features2">
-                                <i className="flaticon-success"></i>
+                                <i className="flaticon-technical-support"></i>
                                 <h3>Built in MFA (multi factor authentication)</h3>
                                 <p>Multi-factor authentication (MFA or 2FA) is an electronic authentication method in which a user is granted access to a website or application only after successfully presenting two or more pieces of evidence (or factors) to an authentication mechanism</p>
                             </div>
                         </div>
                         <div className="col-lg-3">
                             <div className="single-features2">
-                                <i className="flaticon-success"></i>
+                                <i className="flaticon-cyber-security"></i>
                                 <h3>No Single Point of Failure</h3>
                                 <p>A single point of failure (SPOF) is a part of a system that, if it fails, will stop the entire system from working.SPOFs are undesirable in any system with a goal of high availability or reliability, be it a business practice, software application, or other industrial system.</p>
                             </div>
                         </div>
                         <div className="col-lg-3">
                             <div className="single-features2">
-                                <i className="flaticon-success"></i>
+                                <i className="flaticon-deep-learning"></i>
                                 <h3>Cross-platform</h3>
                                 <p>In computing, cross-platform software is computer software that is designed to work in several computing platforms. Some cross-platform software requires a separate build for each platform, but some can be directly run on any platform without special preparation.</p>
                             </div>
                         </div>
                         <div className="col-lg-3">
                             <div className="single-features2">
-                                <i className="flaticon-success"></i>
+                                <i className="flaticon-tick"></i>
                                 <h3>Low code / No code</h3>
                                 <p>Low-code and no-code development platforms are tools for people who either do not know how to code or have no time to code. Whereas these low-code and no-code frameworks are built on actual coding languages like PHP, Python, and Java, end users are not concerned with the specifics</p>
                             </div>
