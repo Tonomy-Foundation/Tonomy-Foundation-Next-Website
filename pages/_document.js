975577f6eabe60fc5317247070326e1bb4dff29e
Karishma-tejwani <karishmatejwani9@gmail.com>
2023-03-20 18:38:39 +0500

    meta-tag robots added

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ class MyDocument extends Document {
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta name="robots" content="index, follow" />
           <link
             href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i&display=swap"
             rel="stylesheet"
