cdc6f5df417c397390969ade841e8908883f282a
Jack Tanner <jack@jackandtheblockstalk.com>
2023-07-18 17:59:54 +0200

    fix: fixed build issue

--- pages/about-1.js
+++ pages/about-1.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from '../components/Layouts/Navbar';
 import PageBanner from '../components/Common/PageBanner';
-import About from '../components/AboutOne/About';
-import MakeYourBusiness from '../components/Common/MakeYourBusiness';
-import Testimonials from '../components/Common/Testimonials';
-import TeamTwo from '../components/Common/TeamTwo';
-import Partner from '../components/Common/Partner';
 import Footer from '../components/Layouts/Footer';
 import ExperienceTonomyId from '../components/tonomy-id/ExperienceTonomyId';
 
