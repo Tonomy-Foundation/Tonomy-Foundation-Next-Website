5a5a6506e31f602d69cec6ed72ab443f7fe43ba3
Jack Tanner <jack@jackandtheblockstalk.com>
2023-09-15 00:11:54 +0200

    build: Change sass options

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const { EDGE_RUNTIME_WEBPACK } = require('next/dist/shared/lib/constants')
 const path = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
   distDir: 'build',
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
+  // sassOptions: {
+  //   includePaths: [path.join(__dirname, 'styles')],
+  // },
   optimizeFonts: false,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
