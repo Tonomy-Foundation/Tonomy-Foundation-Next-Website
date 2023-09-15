941c3c61e06f306c8f2fa89d22abdb2a447bea9c
Jack Tanner <jack@jackandtheblockstalk.com>
2023-09-15 09:47:34 +0200

    feat: Using Bootstrap for table

--- components/UseCases/AntelopeWallet/Comparison.js
+++ components/UseCases/AntelopeWallet/Comparison.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, createTheme, ThemeProvider } from '@mui/material';
+import { Table, tbody, td, TableContainer, thead, tr, Paper, createTheme, ThemeProvider } from '@mui/material';
 
 const theme = createTheme({
     typography: {
@@ -7,99 +7,131 @@ const theme = createTheme({
     },
 });
 
+const ComparisonTable2 = () => {
+    return (
+        <table className="table comparison-table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Larry the Bird</td>
+                    <td>@twitter</td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
+
 const ComparisonTable = () => {
 
     return (
-        <ThemeProvider theme={theme}>
-            <TableContainer component={Paper} className="comparison-table-container">
-                <Table className="comparison-table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell></TableCell>
-                            <TableCell>
-                                <h4>Tonomy ID</h4>
-                                <p>White Labelled Wallet</p>
-                            </TableCell>
-                            <TableCell >
-                                <h4>Anchor</h4>
-                                <p>Multi-Chain Wallet</p></TableCell>
-                            <TableCell >
-                                <h4>Upland</h4>
-                                <p>In-App Web Wallet</p></TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        <TableRow>
-                            <TableCell>Account Creation & Login **</TableCell>
-                            <TableCell>1 min 20 seconds</TableCell>
-                            <TableCell>2 min 30 seconds</TableCell>
-                            <TableCell>1 min 10 seconds</TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell>Wallet Transaction Sign Flow **</TableCell>
-                            <TableCell>15 seconds</TableCell>
-                            <TableCell>15 seconds</TableCell>
-                            <TableCell><i className="bx bx-x"></i></TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell>In-App Transaction Sign Flow</TableCell>
-                            <TableCell>Instant</TableCell>
-                            <TableCell><i className="bx bx-x"></i></TableCell>
-                            <TableCell>Instant</TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell><div>Users Control Keys</div><div>(non-custodial)</div></TableCell>
-                            <TableCell><i className="bx bx-check"></i></TableCell>
-                            <TableCell ><i className="bx bx-check"></i></TableCell>
-                            <TableCell ><i className="bx bx-x"></i></TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell>Wallets</TableCell>
-                            <TableCell>Android, iOS</TableCell>
-                            <TableCell >Android, iOS, Desktop</TableCell>
-                            <TableCell >Web wallet</TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell><div>Free Account Creation</div><div>(User doesn't pay)</div></TableCell>
-                            <TableCell><i className="bx bx-check"></i></TableCell>
-                            <TableCell ><i className="bx bx-x"></i><div>($1)</div></TableCell>
-                            <TableCell ><i className="bx bx-check"></i></TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell><div>Fully Managed Resources</div><div>(RAM, CPU, NET)</div></TableCell>
-                            <TableCell><i className="bx bx-check"></i></TableCell>
-                            <TableCell ><i className="bx bx-x"></i></TableCell>
-                            <TableCell ><i className="bx bx-check"></i></TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell>Recovery</TableCell>
-                            <TableCell><i className="bx bx-check"></i><div>Social, hardware, email (coming soon)</div></TableCell>
-                            <TableCell ><i className="bx bx-check"></i><div>Private Key (certificate) export</div></TableCell>
-                            <TableCell ><i className="bx bx-check"></i><div>Email</div></TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell>Single Sign-On</TableCell>
-                            <TableCell><i className="bx bx-check"></i></TableCell>
-                            <TableCell ><i className="bx bx-check"></i></TableCell>
-                            <TableCell ><i className="bx bx-x"></i></TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell>Self-sovereign Identity</TableCell>
-                            <TableCell><i className="bx bx-check"></i></TableCell>
-                            <TableCell ><i className="bx bx-x"></i></TableCell>
-                            <TableCell ><i className="bx bx-x"></i></TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell>Private Data Storage & Sharing</TableCell>
-                            <TableCell><i className="bx bx-check"></i></TableCell>
-                            <TableCell ><i className="bx bx-x"></i></TableCell>
-                            <TableCell ><i className="bx bx-x"></i></TableCell>
-                        </TableRow>
-                    </TableBody>
-                </Table>
-                <p>**Approximate time with no user errors.</p>
-            </TableContainer >
-        </ThemeProvider>
+        <div className="comparison-table-header">
+            <table className="table comparison-table">
+                <thead>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <h4>Tonomy ID</h4>
+                            <p>White Labelled Wallet</p>
+                        </td>
+                        <td >
+                            <h4>Anchor</h4>
+                            <p>Multi-Chain Wallet</p></td>
+                        <td >
+                            <h4>Upland</h4>
+                            <p>In-App Web Wallet</p></td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Account Creation & Login **</td>
+                        <td>1 min 20 seconds</td>
+                        <td>2 min 30 seconds</td>
+                        <td>1 min 10 seconds</td>
+                    </tr>
+                    <tr>
+                        <td>Wallet Transaction Sign Flow **</td>
+                        <td>15 seconds</td>
+                        <td>15 seconds</td>
+                        <td><i className="bx bx-x"></i></td>
+                    </tr>
+                    <tr>
+                        <td>In-App Transaction Sign Flow</td>
+                        <td>Instant</td>
+                        <td><i className="bx bx-x"></i></td>
+                        <td>Instant</td>
+                    </tr>
+                    <tr>
+                        <td><div>Users Control Keys</div><div>(non-custodial)</div></td>
+                        <td><i className="bx bx-check"></i></td>
+                        <td ><i className="bx bx-check"></i></td>
+                        <td ><i className="bx bx-x"></i></td>
+                    </tr>
+                    <tr>
+                        <td>Wallets</td>
+                        <td>Android, iOS</td>
+                        <td >Android, iOS, Desktop</td>
+                        <td >Web wallet</td>
+                    </tr>
+                    <tr>
+                        <td><div>Free Account Creation</div><div>(User doesn't pay)</div></td>
+                        <td><i className="bx bx-check"></i></td>
+                        <td ><i className="bx bx-x"></i><div>($1)</div></td>
+                        <td ><i className="bx bx-check"></i></td>
+                    </tr>
+                    <tr>
+                        <td><div>Fully Managed Resources</div><div>(RAM, CPU, NET)</div></td>
+                        <td><i className="bx bx-check"></i></td>
+                        <td ><i className="bx bx-x"></i></td>
+                        <td ><i className="bx bx-check"></i></td>
+                    </tr>
+                    <tr>
+                        <td>Recovery</td>
+                        <td><i className="bx bx-check"></i><div>Social, hardware, email (coming soon)</div></td>
+                        <td ><i className="bx bx-check"></i><div>Private key (certificate) export</div></td>
+                        <td ><i className="bx bx-check"></i><div>Email</div></td>
+                    </tr>
+                    <tr>
+                        <td>Single Sign-On</td>
+                        <td><i className="bx bx-check"></i></td>
+                        <td ><i className="bx bx-check"></i></td>
+                        <td ><i className="bx bx-x"></i></td>
+                    </tr>
+                    <tr>
+                        <td>Self-sovereign Identity</td>
+                        <td><i className="bx bx-check"></i></td>
+                        <td ><i className="bx bx-x"></i></td>
+                        <td ><i className="bx bx-x"></i></td>
+                    </tr>
+                    <tr>
+                        <td>Private Data Storage & Sharing</td>
+                        <td><i className="bx bx-check"></i></td>
+                        <td ><i className="bx bx-x"></i></td>
+                        <td ><i className="bx bx-x"></i></td>
+                    </tr>
+                </tbody>
+            </table>
+            <p>**Approximate time with no user errors.</p>
+        </div>
     );
 };
 
@@ -112,9 +144,11 @@ class Comparison extends Component {
                     <div className="section-title">
                         <span>Comparison</span>
                         <h2>Tonomy ID Compared to Other Wallets</h2>
+                        <p>Learn about the different ways wallets can be implemented and what the differences are. Each wallet below has different strengths and weaknesses.</p>
                     </div>
 
                     <ComparisonTable />
+                    <ComparisonTable2 />
 
                 </div>
             </section>
