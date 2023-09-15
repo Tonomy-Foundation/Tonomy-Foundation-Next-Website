6cb1b21a57007c7d26a0c1b37b08346fb423a87d
Jack Tanner <jack@jackandtheblockstalk.com>
2023-09-15 10:26:31 +0200

    fix: Revert next config for sass

--- next.config.js
+++ next.config.js
@@ -6,9 +6,9 @@ const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
   distDir: 'build',
-  // sassOptions: {
-  //   includePaths: [path.join(__dirname, 'styles')],
-  // },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
   optimizeFonts: false,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
