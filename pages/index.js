9813bc4bdf03b66813c6cc2b3644b28266459f3a
Jack Tanner <jack@jackandtheblockstalk.com>
2023-11-07 14:07:21 +0100

    feat: Updated Features and JoinTonomy

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import MainBanner from '../components/HomeOne/MainBanner'
 import Features from '../components/HomeOne/Features'
 import WhyChooseUs from '../components/HomeOne/WhyChooseUs'
 import Services from '../components/HomeOne/Services'
-import WhatWeOffer from "../components/HomeOne/WhatWeOffer"
+import JoinTonomy from "../components/HomeOne/JoinTonomy"
 import Navbar from '../components/Layouts/Navbar'
 import Footer from '../components/Layouts/Footer'
 import Partner from '../components/Common/Partner';
@@ -22,9 +22,9 @@ const Index = () => {
             <WhyChooseUs />
 
             <Services />
-            
-            <WhatWeOffer/>
-            
+
+            <JoinTonomy />
+
             <Partner />
 
             <Footer />
