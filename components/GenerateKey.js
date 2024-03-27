b667715c6ae45fb8e267ce17dcb69cf6d396bfdd
Jack Tanner <jack@jackandtheblockstalk.com>
2024-03-27 16:51:29 +0100

    feat: Generates a new private key

--- components/GenerateKey.js
+++ components/GenerateKey.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { KeyType, PrivateKey } from "@wharfkit/antelope"
 
 export default function GenerateKey() {
+    const privateKey = PrivateKey.generate(KeyType.K1);
+    const publicKey = privateKey.toPublic();
+
     return (
         <section className="team-area pt-100 pb-70">
             <div className="container-fluid">
@@ -8,8 +12,8 @@ export default function GenerateKey() {
                     <h2>Refresh Page to Generate a new Private Key</h2>
                 </div>
                 <div className="section-title">
-                    <p>Private Key: </p>
-                    <p>Public Key: </p>
+                    <p>Private Key: {privateKey.toString()}</p>
+                    <p>Public Key: {publicKey.toString()}</p>
                 </div>
             </div>
         </section>
