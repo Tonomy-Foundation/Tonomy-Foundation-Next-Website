dea246818e0389030159342e26259444295d6719
Jack Tanner <jack@jackandtheblockstalk.com>
2024-03-27 16:44:53 +0100

    feat: Generate a private key

---
+++ components/GenerateKey.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function GenerateKey() {
+    return (
+        <section className="team-area pt-100 pb-70">
+            <div className="container-fluid">
+                <div className="section-title">
+                    <h2>Refresh Page to Generate a new Private Key</h2>
+                </div>
+                <div className="section-title">
+                    <p>Private Key: </p>
+                    <p>Public Key: </p>
+                </div>
+            </div>
+        </section>
+    );
+}
